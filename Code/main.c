53b20ba1ef024ec35068230cd280506012b938da
raghavendra495 <53701156+raghavendra495@users.noreply.github.com>
2019-10-31 13:41:05 +0530

    Update main.c

--- Code/main.c
+++ Code/main.c
@@ -80,4 +80,43 @@ void AllocateLRU( int addr, CACHEINT *cache, int memtype ){
 		(*cache).Cache_DATE[X] = (*cache).ACCESS;
 }
 
+int Is_In_CacheASSOC( int addr, CACHEINT *cache, int memtype ){
+	int i, X, T, inter;
+	
+	T = (*cache).Size_Bank;
+	X = (addr & (T - 1)) * (*cache).Number_of_Bank;
+	
+	if ((*cache).Cache[X] == addr) return(1);
+	
+	for (i = 1; i < (*cache).Number_of_Bank; i++){
+		if ((*cache).Cache[X + i] == addr) {
+			(*cache).Cache_DATE[X + i] = (*cache).Cache_DATE[X];
+			(*cache).Cache_DATE[X]= (*cache).ACCESS;
+			inter = (*cache).Cache[X];
+			(*cache).Cache[X]= (*cache).Cache[X + i];
+			(*cache).Cache[X + i]= inter;
+			
+			return (1);
+		}
+	}
+	return (0);
+}
+
+
+int main(int argc, char** argv)
+{
+  int memtype;
+  int addr;
+
+  INITCACHE(argc, &argv[0]);
+	
+  while (scanf("%d %x", &memtype, &addr)>=0) {
+    memory_access(memtype, addr);
+  };
+
+  SIM();
+  DUMPRES();
+
+  return 0;
+}
 
