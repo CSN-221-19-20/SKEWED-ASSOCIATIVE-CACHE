627d6d75a735601a26974f316731c2b6daf5447f
GJIITR <48017576+GJIITR@users.noreply.github.com>
2019-10-31 10:32:34 +0530

    Add files via upload

--- Code/SkewFile.c
+++ Code/SkewFile.c
@@ -52,3 +52,91 @@ int Is_In_CacheSKEWUseful( int addr, CACHEINT *cache, int memtype ){
 	}
 	return (0);
 }
+
+void AllocateSKEWpseudoLRU(int, CACHEINT*, int);
+int Is_In_CacheSKEWpseudoLRU(int, CACHEINT*, int);
+
+void AllocateSKEWpseudoLRU(int addr, CACHEINT *cache, int memtype ){
+	int		i, set;
+	Mapfct( addr, cache, memtype );
+	set = (*cache).Cache_Prio[AdBank[0]];
+	
+	for (i = 0; i < (*cache).Number_of_Bank; i++)
+		if ((*cache).Cache[AdBank[i]] == FreeAddr) 
+		set = i;
+	
+	#ifdef SIML2
+		EXCLUDE= (*cache).Cache[AdBank[set]];
+	#endif
+	(*cache).Cache[AdBank[set]] = addr;
+	(*cache).Cache_Prio[AdBank[0]] = 1 - set;
+}
+
+int Is_In_CacheSKEWpseudoLRU(int addr, CACHEINT *cache, int memtype ){
+	int	i;
+	
+	if ((*cache).Number_of_Bank != 2){
+		printf("ERROR: The SKEWpseudoLRU replacement policy only works for two banks.");
+		exit(0);
+	}
+
+	Mapfct( addr, cache, memtype );
+	
+	for (i = 0; i < (*cache).Number_of_Bank; i++){
+		if ((*cache).Cache[AdBank[i]] == addr) {
+			(*cache).Cache_Prio[AdBank[0]] = 1 - i;
+			return (1);
+		}
+	}
+	return (0);
+}
+
+void AllocateSKEWENRU(int, CACHEINT*, int);
+int Is_In_CacheSKEWENRU(int, CACHEINT*, int);
+
+void AllocateSKEWENRU(int addr, CACHEINT *cache, int  memtype ){
+	int Age[8], set, i, Start, pseudoDATE, pseudoDATE1;
+	
+	Start = random() & ((*cache).Number_of_Bank - 1);
+	
+	Mapfct( addr, cache, memtype );
+	set = -1;
+	
+	pseudoDATE = (*cache).DATE & (0xffffffff - (((*cache).Size / 2) - 1));
+	pseudoDATE1 = ((*cache).DATE +  ((*cache).Size / 4)) & (0xffffffff - ((*cache).Size / 2) - 1);
+
+	for(i=0; i<(*cache).Number_of_Bank; i++)
+		Age[i] = (((*cache).Cache_DATE[AdBank[i]] < pseudoDATE) & (((*cache).Cache_DATE[AdBank[i]] + ((*cache).Size / 4)) < pseudoDATE1));
+
+	for(i=Start; i<(*cache).Number_of_Bank; i++)
+		if (Age[i]) set=i;
+	
+	for(i=0; i<Start; i++)
+		if (Age[i]) set=i;
+	
+	if (set == -1){
+		for(i=0; i<(*cache).Number_of_Bank; i++)
+			Age[i] = (((*cache).Cache_DATE[AdBank[i]] < pseudoDATE) |  ((((*cache).Cache_DATE[AdBank[i]] + ((*cache).Size / 4))) < pseudoDATE1));
+		
+		for(i=Start; i<(*cache).Number_of_Bank; i++)
+			if (Age[i]) set=i;
+		
+		for(i=0; i<Start; i++)
+			if (Age[i]) set=i;
+	}
+	
+	if (set== -1)
+		set = Start;
+	
+	for (i = 0; i < (*cache).Number_of_Bank; i++)
+		if ((*cache).Cache[AdBank[i]] == FreeAddr) 
+			set = i;
+		
+	(*cache).Cache_DATE[AdBank[set]] = (*cache).DATE;
+	(*cache).DATE++;
+	#ifdef SIML2
+		EXCLUDE= (*cache).Cache[AdBank[set]];
+	#endif
+
+	(*cache).Cache[AdBank[set]] = addr;
+}
