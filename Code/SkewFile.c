548f227718c3e2724d013f8d4ec93911eac0f598
GJIITR <48017576+GJIITR@users.noreply.github.com>
2019-10-31 17:45:49 +0530

    Add files via upload

--- Code/SkewFile.c
+++ Code/SkewFile.c
@@ -140,3 +140,114 @@ void AllocateSKEWENRU(int addr, CACHEINT *cache, int  memtype ){
 
 	(*cache).Cache[AdBank[set]] = addr;
 }
+
+
+int Is_In_CacheSKEWENRU( int addr, CACHEINT *cache, int memtype ){
+	int	i;
+	
+	Mapfct( addr, cache, memtype );
+	
+	for (i = 0; i < (*cache).Number_of_Bank; i++) {
+		if ((*cache).Cache[AdBank[i]] == addr) {
+			if ((*cache).Cache_DATE[AdBank[i]] < ((*cache).DATE & (0xffffffff - ((*cache).Size / 2 - 1))))
+				(*cache).DATE++;
+			
+			(*cache).Cache_DATE[AdBank[i]] = (*cache).DATE;
+			return (1);
+		}
+	}
+	return (0);
+}
+
+void AllocateSKEWLRU(int, CACHEINT*, int);
+int Is_In_CacheSKEWLRU(int, CACHEINT*, int);
+
+void AllocateSKEWLRU( int addr, CACHEINT *cache, int memtype ){
+	int	i, min, set;
+	
+	Mapfct( addr, cache, memtype );
+	
+	set = 0;
+	min = (*cache).Cache_DATE[AdBank[0]];
+	for (i = 1; i < (*cache).Number_of_Bank; i++){
+		if ((*cache).Cache_DATE[AdBank[i]] < min) {
+			set = i;
+			min = (*cache).Cache_DATE[AdBank[i]];
+		}
+	}
+	for (i = 0; i < (*cache).Number_of_Bank; i++){
+		if ((*cache).Cache[AdBank[i]] == FreeAddr)
+			set = i;
+	}
+	#ifdef SIML2
+		EXCLUDE= (*cache).Cache[AdBank[set]];
+	#endif
+
+	(*cache).Cache[AdBank[set]]= addr;
+	(*cache).Cache_DATE[AdBank[set]]= (*cache).ACCESS ;
+}
+
+int Is_In_CacheSKEWLRU( int addr, CACHEINT *cache, int memtype ){
+	int	i;
+	Mapfct( addr, cache, memtype );
+	
+	for (i = 0; i < (*cache).Number_of_Bank; i++){
+		if ((*cache).Cache[AdBank[i]] == addr) {
+			(*cache).Cache_DATE[AdBank[i]] = (*cache).ACCESS;
+			return (1);
+		}
+	}
+	return (0);
+}
+
+void AllocateSKEWNRU(int, CACHEINT*, int);
+int Is_In_CacheSKEWNRU(int, CACHEINT*, int);
+
+void AllocateSKEWNRU( int addr, CACHEINT *cache, int memtype ){
+	int Old[8], set, i, Start, pseudoDATE;
+	
+	Start = random() & ((*cache).Number_of_Bank - 1);
+	Mapfct( addr, cache, memtype );
+	set = -1;
+	pseudoDATE = (*cache).DATE & (0xffffffff - (((*cache).Size / 2) - 1));
+	
+	for(i=0; i<(*cache).Number_of_Bank; i++)
+		Old[i] = (((*cache).Cache_DATE[AdBank[i]] < pseudoDATE));
+	
+	for(i=Start; i<(*cache).Number_of_Bank; i++)
+		if (Old[i]) set=i;
+	
+	for(i=0; i<Start; i++)
+		if (Old[i]) set=i;
+	
+	if (set== -1) 
+		set =Start;
+	
+	for (i = 0; i < (*cache).Number_of_Bank; i++)
+		if ((*cache).Cache[AdBank[i]] == FreeAddr) 
+			set = i;
+	
+	(*cache).Cache_DATE[AdBank[set]] = (*cache).DATE;
+	(*cache).DATE++;
+	#ifdef SIML2
+		EXCLUDE= (*cache).Cache[AdBank[set]];
+	#endif
+	(*cache).Cache[AdBank[set]] = addr;
+}
+
+int Is_In_CacheSKEWNRU( int addr, CACHEINT *cache, int memtype ){
+  int	i;
+  
+  Mapfct( addr, cache, memtype );
+  
+  for (i = 0; i < (*cache).Number_of_Bank; i++) {
+    if ((*cache).Cache[AdBank[i]] == addr) {
+      	if ((*cache).Cache_DATE[AdBank[i]] < ((*cache).DATE & (0xffffffff - ((*cache).Size / 2 - 1))))
+			(*cache).DATE++;
+      
+		(*cache).Cache_DATE[AdBank[i]] = (*cache).DATE;
+		return (1);
+    }
+  };
+  return (0);
+}
